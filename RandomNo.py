552c201f387d37a6b026513d7daca4af654d85af
Muhammad Ahsan <180093008+Muhammad-Farhan1@users.noreply.github.com>
2025-07-13 15:45:46 +0500

    Add comments and docstring to RandomNo.py for clarity on random number functions

--- RandomNo.py
+++ RandomNo.py
@@ -1,3 +1,5 @@
+#Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers:
+
 import random
 low = 1
 high = 100
@@ -11,3 +13,9 @@ list = ["0" , "2" , "3" ,"4" , "A" , "G" , "H"]
 #print(list)
 
 print(random.randint(1,10))
+
+'''
+randrange() -> Returns a random number between the given range
+randint() ->	Returns a random number between the given range
+shuffle() ->	Takes a sequence and returns the sequence in a random order
+'''
