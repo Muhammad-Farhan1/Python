8f2f3ee3c663d67f5cd1af54c5d3d72124a9ce2e
Muhammad Ahsan <180093008+Muhammad-Farhan1@users.noreply.github.com>
2025-07-13 15:37:10 +0500

    Add variables.py with initial variable definitions and rules for Python variables

--- varbles.py
+++
@@ -1,7 +0,0 @@
-age = 10
-height = 5.7
-name = "john"
-f_name = "wick"
-is_inter_done = True
-intro = f"My name is {name} and my father name is {f_name} while my age is {age} and my height is {height} and my intermediate has been done {is_inter_done}"
-print(intro) 
---
+++ variables.py
@@ -0,0 +1,26 @@
+#Variables -> Variables are containers for storing data values.
+
+age = 10   
+height = 5.7
+name = "john"
+f_name = "wick"
+is_inter_done = True
+intro = f"My name is {name} and my father name is {f_name} while my age is {age} and my height is {height} and my intermediate has been done {is_inter_done}"
+print(intro) 
+
+
+'''
+Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+'''
+
+#The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = 5
+y = "John"
+print(x, y)
+#Note : int and str can't be added 
+
